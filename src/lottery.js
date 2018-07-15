03a8a4200aa285975f5c3f999b9985ed604588f6
John Leonardo <jdleonardo5@icloud.com>
2018-07-14 19:47:29 -0700

    web app shows contract manager

--- src/lottery.js
+++ src/lottery.js
@@ -1,20 +1,68 @@
-import web3 from './web3';
+import web3 from "./web3";
 
-const address = '0xff3a14ca7a3EfaF40aFcaAfd10a4420F68402E12';
+const address = "0xff3a14ca7a3EfaF40aFcaAfd10a4420F68402E12";
 
-const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name
-":"","type":"address"}],"payable":false,"stateMutability":"view","type":"f
-unction"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"
-payable":false,"stateMutability":"nonpayable","type":"function"},{"constan
-t":true,"inputs":[],"name":"random","outputs":[{"name":"","type":"uint256"
-}],"payable":false,"stateMutability":"view","type":"function"},{"constant"
-:false,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"addre
-ss[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},
-{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"
-stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{
-"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type"
-:"address"}],"payable":false,"stateMutability":"view","type":"function"},{
-"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constru
-ctor"}]
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "random",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
 
 export default new web3.eth.Contract(abi, address);
